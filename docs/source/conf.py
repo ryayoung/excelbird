d9d884328a5c1376f4ee59fef6d3b966eb2f2e1a
ryayoung <ryan.d.young@du.edu>
2023-02-03 12:54:58 -0700

    updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -151,8 +151,8 @@ html_theme_options = dict(
     show_toc_level = 2,
     # Add light/dark mode and documentation version switcher:
     # "navbar_end": ["theme-switcher", "version-switcher", "navbar-icon-links"],
-    pygment_light_style="manni",
-    pygment_dark_style="material",
+    pygment_light_style="github-dark",
+    pygment_dark_style="github-dark",
 )
 
 
