e1bd1680354768ecfb1df8d9dad803f759fe17a8
Ryan Young <90723578+ryayoung@users.noreply.github.com>
2023-02-05 02:28:57 -0700

    Update test.py

---
+++ tests/test.py
@@ -0,0 +1,18 @@
+from excelbird import Func
+lst = [1, 2, 3]
+
+print("Hello world")
+print(f"F {lst[:2]}")
+d = dict(
+    a=5,
+    b=lst,
+)
+d2 = {
+    "one": "two",
+}
+f = Func(one=1, two=2)
+
+
+stuff goes here 
+
+s = "SUM({{one + 5}})"
